41711e1d98e015098e09f756781c59ce338a855f
Monty <tuckerjsarah@gmail.com>
2019-01-21 13:42:14 -0500

    addmore

--- assets/api.js
+++ assets/api.js
@@ -1,5 +1,8 @@
 var apiKey= "pqNOnbzfBte9skLM2LpXsdEXxeYh9GCz";
 var policians = ["Barack Obama", "Donald Trump", "Hillary Clinton", "Bill Clinton", "Joe Biden", "Vladimir Putin", "Sarah Palin", "Bernie Sanders", "George W. Bush", "John McCain"];
+var poliGif;
+var limit = 15;
+
 
 function btnsOnTheFly() {
     $("#myBtns").empty();
@@ -22,8 +25,7 @@ $("#addGif").click(function(event) {
 
 function giveMeGif () {
     $("#gifs").empty();
-    var poliGif = $(this).attr("data-name");
-    var limit = 15;
+    poliGif = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key="+apiKey+"&q="+poliGif+"&limit="+limit+"&offset=0&rating=PG13&lang=en";
     
     $.ajax({url:queryURL, method: "GET"})
@@ -53,46 +55,46 @@ function giveMeGif () {
             div.append(image);
             div.append(pDiv);
             $("#gifs").append(div);
+            $("#moreGifs").css("opacity", "1");
         };
         
-    });
-    $("#moreGifs").click(function(){
-        $("#gifs").empty();
-        limit= limit+6;
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key="+apiKey+"&q="+poliGif+"&limit="+limit+"&offset=0&rating=PG13&lang=en";
-        $.ajax({url:queryURL, method: "GET"})
-        .then(function(response){
-        console.log(response);
-        var results = response.data;
-
-        for (var i = 0; i < results.length; i++) {
-            var div = $("<div>");
-            div.addClass("myCard float-left card text-white bg-info mb-3");
-            var pDiv = $("<div>");
-            pDiv.addClass("card-text");
-            var rating = results[i].rating;
-            var title = results[i].title;
-            var p = $("<p>").text(title);
-            var p2 = $("<p>").text("Rating: " + rating);
-            p.addClass("title card-text");
-            p2.addClass("ratings card-text");
-            pDiv.append(p);
-            pDiv.append(p2);
-            var image = $("<img>");
-            image.attr("src", results[i].images.fixed_height_still.url);
-            image.attr("data-still", results[i].images.fixed_height_still.url);
-            image.attr("data-animate", results[i].images.fixed_height.url);
-            image.attr("data-state", "still");
-            image.addClass("gif card-img-top");
-            div.append(image);
-            div.append(pDiv);
-            $("#gifs").append(div);
-        };
-        
-    });
     });
 }
+$("#moreGifs").click(function(){
+    $("#gifs").empty();
+    limit= limit+9;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key="+apiKey+"&q="+poliGif+"&limit="+limit+"&offset=0&rating=PG13&lang=en";
+    $.ajax({url:queryURL, method: "GET"})
+    .then(function(response){
+    console.log(response);
+    var results = response.data;
 
+    for (var i = 0; i < results.length; i++) {
+        var div = $("<div>");
+        div.addClass("myCard float-left card text-white bg-info mb-3");
+        var pDiv = $("<div>");
+        pDiv.addClass("card-text");
+        var rating = results[i].rating;
+        var title = results[i].title;
+        var p = $("<p>").text(title);
+        var p2 = $("<p>").text("Rating: " + rating);
+        p.addClass("title card-text");
+        p2.addClass("ratings card-text");
+        pDiv.append(p);
+        pDiv.append(p2);
+        var image = $("<img>");
+        image.attr("src", results[i].images.fixed_height_still.url);
+        image.attr("data-still", results[i].images.fixed_height_still.url);
+        image.attr("data-animate", results[i].images.fixed_height.url);
+        image.attr("data-state", "still");
+        image.addClass("gif card-img-top");
+        div.append(image);
+        div.append(pDiv);
+        $("#gifs").append(div);
+    };
+    
+});
+});
 
 $(document).on("click", ".myBtn", giveMeGif);
     
